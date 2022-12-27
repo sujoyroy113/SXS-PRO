461a039928f95a9ece53d744753cde55ec22904e
MR. SUJOY <110232390+sujoyroy113@users.noreply.github.com>
2022-12-27 11:58:09 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,3 +1,3 @@
-print('\032[1;34m\n Congratulations! Your Device Support This Tools\032[1;37m')
+print('\033[1;32m\n Congratulations! Your Device Support This Tools\033[1;32m')
 import SXS_PRO
 SXS_PRO.Subscraption()
