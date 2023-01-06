4360c77da39afd62eb99e209f27c4a44a6c60bfe
MR. SUJOY <110232390+sujoyroy113@users.noreply.github.com>
2023-01-06 21:13:37 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -8,14 +8,9 @@ white = ('\033[1;37m')
 red = ('\033[1;31m')
 
 print('<------------------------------------>')
-bit = platform.architecture()[0]
-if bit=='64bit':
     print(f'{red}[•] Join Over Facebook Group {white}')
     os.system('xdg-open https://facebook.com/groups/412423917720633//')
     time.sleep(0.05)
     import SXS_PRO
 SXS_PRO.Subscraption()   
-elif bit=='32bit':
-    import trt32
-else:
-    print(f'{green}[×] Sorry System Not Support{white}')
+
